9ca32ceb851dba7e62e9bb644025748e97d65016
Ronald Holshausen <rholshausen@dius.com.au>
2021-06-12 11:19:07 +1000

    chore: upgrade pact-jvm libs

--- project.clj
+++ project.clj
@@ -1,22 +1,20 @@
-(defproject pact-lein-test "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject pact-lein-test "0.1.0"
+  :description "test lein project"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
     [org.clojure/clojure "1.8.0"]
-    ; [jansi-clj "0.1.0"]
   ]
   :main ^:skip-aot pact-lein-test.core
   :target-path "target/%s"
   :profiles {
     :uberjar {:aot :all}
     :pact {
-            :plugins [[au.com.dius/pact-jvm-provider-lein_2.11 "3.2.11" :exclusions [commons-logging]]]
-            :dependencies [[ch.qos.logback/logback-core "1.1.3"]
-                           [ch.qos.logback/logback-classic "1.1.3"]
-                           [org.apache.httpcomponents/httpclient "4.4.1"]
-                          ;  [jansi-clj "0.1.0"]
+            :plugins [[au.com.dius.pact.provider/lein "4.1.20" :exclusions [commons-logging]]]
+            :dependencies [[ch.qos.logback/logback-core "1.2.3"]
+                           [ch.qos.logback/logback-classic "1.2.3"]
+                          ;;  [org.apache.httpcomponents/httpclient "4.4.1"]
                            ]
     }
   }
@@ -35,7 +33,7 @@
                   ; Again, you can define as many consumers for each provider as you need, but each must have a unique name
                   :consumer1 {
                     ; pact file can be either a path or an URL
-                    :pact-file "file:test/sample-pact.json"
+                    :pact-source "file:test/sample-pact.json"
                   }
               }
           }
